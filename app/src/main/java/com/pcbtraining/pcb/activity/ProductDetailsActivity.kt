9ee802ca9cfab27ee78e7617c1e5cae7b71e3961
SuperBytesApp <161282609+SuperBytesApp@users.noreply.github.com>
2024-09-13 18:14:03 +0530

    13 sept

---
+++ app/src/main/java/com/pcbtraining/pcb/activity/ProductDetailsActivity.kt
@@ -0,0 +1,16 @@
+package com.pcbtraining.pcb.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pcbtraining.pcb.R
+
+class ProductDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product_details)
+
+
+
+
+    }
+}
